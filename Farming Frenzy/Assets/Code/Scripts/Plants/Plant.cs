29f2edbd8eef4ac3b24f355413b6970b1504d44c
Tomas Cupido <tomascupidol@gmail.com>
2024-10-09 12:42:25 +0200

    updated plant

--- Farming Frenzy/Assets/Code/Scripts/Plants/Plant.cs
+++ Farming Frenzy/Assets/Code/Scripts/Plants/Plant.cs
@@ -1,5 +1,5 @@
-using System.Collections;
 using System;
+using System.Collections;
 using Code.Scripts.Plants.GrowthStateExtension;
 using Code.Scripts.Plants.Powers;
 using Code.Scripts.Plants.Powers.PowerExtension;
@@ -8,45 +8,6 @@ using UnityEngine.EventSystems;
 
 namespace Code.Scripts.Plants
 {
-    #region Editor Fields
-
-    #endregion
-
-    #region Properties
-    private PlantData data;
-    private Sprite currentSprite;
-    private float time;
-    private enum GrowthState {Start, Growing, Finished, Harvested};
-    private GrowthState state;
-    private SpriteRenderer _plantSpriteRenderer;
-    private int _growthSpriteIndex;
-    private float _health;
-    private bool _readyToHarvest;
-    private Coroutine _damageCoroutine;
-    #endregion
-
-    #region Methods
-    private void Awake()
-    {
-        _plantSpriteRenderer = GetComponent<SpriteRenderer>();   
-    }
-
-    void Update()
-    {
-        UpdateState();
-        if(_health <= 0)
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    public void InitPlant(PlantData _pdata)
-    {
-        data = _pdata;
-        state = GrowthState.Start;
-        time = Time.time;
-        _health = _pdata._health;
-        if(data._isTree)
     public class Plant : MonoBehaviour
     {
         #region Properties
@@ -59,7 +20,9 @@ namespace Code.Scripts.Plants
         private bool _readyToHarvest;
         public BoxCollider2D Collider { get; private set; }
         private float _growthRate;
-    
+        private Coroutine _damageCoroutine;
+        private float _health;
+
         private int SecsToNextStage
         {
             get
@@ -87,7 +50,7 @@ namespace Code.Scripts.Plants
         #region Methods
         private void Awake()
         {
-            _plantSpriteRenderer = GetComponent<SpriteRenderer>();   
+            _plantSpriteRenderer = GetComponent<SpriteRenderer>();
         }
 
         void Update()
@@ -101,8 +64,8 @@ namespace Code.Scripts.Plants
             _state = GrowthState.Seedling;
             _secsSinceGrowth = 0.0f;
             Collider = GetComponent<BoxCollider2D>();
-
-            if(_data._isTree)
+            _health = pdata._health;
+            if (_data._isTree)
             {
                 Collider.size = new Vector2(3, 2);
                 Collider.offset = new Vector2(0, 0.5f);
@@ -119,17 +82,18 @@ namespace Code.Scripts.Plants
             switch (_state)
             {
                 case GrowthState.Seedling:
-                    _secsSinceGrowth += Time.deltaTime * _growthRate; 
+                    _secsSinceGrowth += Time.deltaTime * _growthRate;
 
                     if (_secsSinceGrowth <= _data._maturationCycle)
                     {
                         var spriteIndex = Mathf.FloorToInt(_secsSinceGrowth * _data._maturationSprite.Length / _data._maturationCycle);
                         _plantSpriteRenderer.sprite = _data._maturationSprite[spriteIndex];
-                    } else 
+                    }
+                    else
                     {
                         _state = GrowthState.Mature;
                         _data.power.AddTo(gameObject); // Power only enabled when the plant is grown
-                        _secsSinceGrowth = 0; 
+                        _secsSinceGrowth = 0;
                     }
                     break;
                 case GrowthState.Mature:
@@ -139,12 +103,12 @@ namespace Code.Scripts.Plants
                         break;
                     }
 
-                    _secsSinceGrowth += Time.deltaTime; 
+                    _secsSinceGrowth += Time.deltaTime;
 
                     if (_secsSinceGrowth <= _data._fruitingCycle)
                     {
                         var spriteIndex = Mathf.FloorToInt(_secsSinceGrowth * _data._growthSprite.Length / _data._fruitingCycle);
-                        if(spriteIndex > 0) { _plantSpriteRenderer.sprite = _data._growthSprite[spriteIndex - 1];}
+                        if (spriteIndex > 0) { _plantSpriteRenderer.sprite = _data._growthSprite[spriteIndex - 1]; }
                     }
                     else
                     {
@@ -185,44 +149,40 @@ namespace Code.Scripts.Plants
             OnHoverOut?.Invoke(this);
         }
 
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Enemy"))
+            {
+                if (_damageCoroutine != null)
+                {
+                    StopCoroutine(_damageCoroutine);
+
+                }
+                _damageCoroutine = StartCoroutine(TakeAnimalDamage());
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag("Enemy"))
+            {
+                if (_damageCoroutine != null)
+                {
+                    StopCoroutine(_damageCoroutine);
+
+                }
+                _damageCoroutine = StartCoroutine(TakeAnimalDamage());
+            }
+        }
+
+        private IEnumerator TakeAnimalDamage()
+        {
+            while (true)
+            {
+                _health -= 5;
+                yield return new WaitForSeconds(1f);
+            }
+        }
         #endregion
     }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if(other.CompareTag("Enemy"))
-        {
-            if(_damageCoroutine != null)
-            {
-                StopCoroutine(_damageCoroutine);
-
-            }
-            _damageCoroutine = StartCoroutine(TakeAnimalDamage());
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Enemy"))
-        {
-            if (_damageCoroutine != null)
-            {
-                StopCoroutine(_damageCoroutine);
-
-            }
-            _damageCoroutine = StartCoroutine(TakeAnimalDamage());
-        }
-    }
-
-    private IEnumerator TakeAnimalDamage()
-    {
-        while(true)
-        {
-            Debug.Log($"Damage: {_health}");
-            _health -= 5;
-            yield return new WaitForSeconds(1f);
-        }
-    }
-    #endregion
-}
 }
